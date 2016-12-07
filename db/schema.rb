4c8ff439cb1f39313c4f148d700bc7275b7bf485
Vas Kaloidis <vas.kaloidis@gmail.com>
2016-12-07 00:55:20 -0500

    Updated Schema by deleting migratioons and a db:reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202062533) do
+ActiveRecord::Schema.define(version: 20161207044100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20161202062533) do
     t.string   "name"
     t.string   "api_key"
     t.text     "api_secret"
-    t.datetime "created_at", :null=>false
-    t.datetime "updated_at", :null=>false
+    t.datetime "created_at",    :null=>false
+    t.datetime "updated_at",    :null=>false
+    t.boolean  "BuilderLocked"
   end
 
   create_table "configs", force: :cascade do |t|
