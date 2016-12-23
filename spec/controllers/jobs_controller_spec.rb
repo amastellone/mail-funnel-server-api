6ec30665610ec86415e4a5b40cded822436af9ac
Matt Twardowski <mttwardowski@gmail.com>
2016-12-23 14:31:28 -0600

    Modified circle.yml file to run test cases upon push

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -42,11 +42,17 @@ RSpec.describe JobsController, type: :controller do
   let(:valid_session) { {} }
 
   describe "GET #index" do
+
+    # Test to see if logger has gotten
+
+    # Generated Automatically by RSpec
     it "returns a success response" do
       job = Job.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(response).to be_success
     end
+
+
   end
 
   describe "GET #show" do
