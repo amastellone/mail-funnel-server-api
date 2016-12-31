59c60421006086d9017146eb61531a8d78a6f10c
Vas Kaloidis <vas.kaloidis@gmail.com>
2016-12-30 23:06:24 -0500

    Added TODO In controller for security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
 	# binding.pry
+
+	# TODO Security - Secure API Controllers from forgery with :null_session
+
   # protect_from_forgery with: :exception
 end
