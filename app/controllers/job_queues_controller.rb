8585340a6a65b23de2c54d64492202b202eaa8ca
Matt Twardowski <mttwardowski@gmail.com>
2017-02-13 17:51:49 -0500

    get sidekiq job status working

--- app/controllers/job_queues_controller.rb
+++ app/controllers/job_queues_controller.rb
@@ -1,18 +1,18 @@
+require 'json'
+
 class JobQueuesController < ApplicationController
 
 	def index
 
 		if params.has_key?(:queue_identifier)
-			# Code to sidekiq
-			queued_job = Sidekiq::Status::get_all params[:queue_identifier]
-			if queued_job
-				return queued_job
-			else
-				logger.error "This Job does not exists in Sidekiq"
-			end
+			# fetch job status from sidekiq-status
+			status = Sidekiq::Status::get_all(params[:queue_identifier])
+    		logger.debug "The status of this job is: " + status['status']
+    		my_hash = {:status => status['status']}
+			render :json => status['status']
 		else
 			logger.error "Jobs-Queue-Controller GET Queued Jobs - no queue_identifier was provided"
-			return 'Must pass-in an app-id'
+			return 'Must pass-in an queue_identifier'
 		end
 
 	end
